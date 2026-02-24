a11fee00b6a5dc177b6a39578a6783d12747924b
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2026-02-24 12:17:54 +0530

    [Ishan Shrivastava] added: Quantity Measurement App

--- src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -3,10 +3,13 @@ package com.apps.quantitymeasurement;
 
 //main class
 public class QuantityMeasurementApp {
+
+	// method to Demonstrate Length Equality
 	public static boolean demonstrateLengthEquality(Length l1, Length l2) {
 		return l1.equals(l2);
 	}
 
+	// Method to Demonstrate Length conversion
 	public static double demonstrateLengthConversion(double value, LengthUnit fromUnit, LengthUnit toUnit) {
 		return Length.convert(value, fromUnit, toUnit);
 	}
@@ -17,13 +20,18 @@ public class QuantityMeasurementApp {
 
 	public static void main(String[] args) {
 
-
+		//defining two length
 		Length l1 = new Length(1.0, LengthUnit.FEET);
 		Length l2 = new Length(12.0, LengthUnit.INCHES);
 
-		System.out.println(Length.add(l1, l2, LengthUnit.FEET));
-		System.out.println(Length.add(l1, l2, LengthUnit.INCHES));
-		System.out.println(Length.add(l1, l2, LengthUnit.YARDS));
-		System.out.println(Length.add(l1, l2, LengthUnit.CENTIMETERS));
+		printResult("FEET", Length.add(l1, l2, LengthUnit.FEET));
+		printResult("INCHES", Length.add(l1, l2, LengthUnit.INCHES));
+		printResult("YARDS", Length.add(l1, l2, LengthUnit.YARDS));
+		printResult("CENTIMETERS", Length.add(l1, l2, LengthUnit.CENTIMETERS));
+	}
+
+	//method to print result
+	private static void printResult(String label, Length result) {
+		System.out.println(label + " = " + result);
 	}
 }
