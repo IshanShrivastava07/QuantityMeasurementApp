f626a960080d01a1ac4f15475869ef03e4628de8
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2026-02-27 16:12:05 +0530

    [Ishan Shrivastava] added Weight Measurement App

--- src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -1,4 +1,3 @@
-
 package com.apps.quantitymeasurement;
 
 //main class
@@ -18,9 +17,27 @@ public class QuantityMeasurementApp {
 		return length.convertTo(toUnit);
 	}
 
+	// ---------------- WEIGHT METHODS ----------------
+
+	public static boolean demonstrateWeightEquality(Weight w1, Weight w2) {
+		return w1.equals(w2);
+	}
+
+	public static Weight demonstrateWeightConversion(Weight weight, WeightUnit targetUnit) {
+		return weight.convertTo(targetUnit);
+	}
+
+	public static Weight demonstrateWeightAddition(Weight w1, Weight w2) {
+		return w1.add(w2);
+	}
+
+	public static Weight demonstrateWeightAddition(Weight w1, Weight w2, WeightUnit targetUnit) {
+		return Weight.add(w1, w2, targetUnit);
+	}
+
 	public static void main(String[] args) {
 
-		//defining two length
+		// defining two length
 		Length l1 = new Length(1.0, LengthUnit.FEET);
 		Length l2 = new Length(12.0, LengthUnit.INCHES);
 
@@ -28,10 +45,30 @@ public class QuantityMeasurementApp {
 		printResult("INCHES", Length.add(l1, l2, LengthUnit.INCHES));
 		printResult("YARDS", Length.add(l1, l2, LengthUnit.YARDS));
 		printResult("CENTIMETERS", Length.add(l1, l2, LengthUnit.CENTIMETERS));
+
+		// ---------------- WEIGHT DEMO ----------------
+
+		System.out.println("\nWeight Equality:");
+
+		Weight w1 = new Weight(1.0, WeightUnit.KILOGRAM);
+		Weight w2 = new Weight(1000.0, WeightUnit.GRAM);
+
+		System.out.println("1 KG equals 1000 G ? " + demonstrateWeightEquality(w1, w2));
+
+		System.out.println("\nWeight Conversion:");
+		Weight pounds = new Weight(2.0, WeightUnit.POUND);
+		System.out.println("2 LB in KG = " + demonstrateWeightConversion(pounds, WeightUnit.KILOGRAM));
+
+		System.out.println("\nWeight Addition (Implicit Target)");
+		System.out.println("1 KG + 1000 G = " + demonstrateWeightAddition(w1, w2));
+
+		System.out.println("\nWeight Addition (Explicit Target - POUND)");
+		System.out.println("1 KG + 1000 G in LB = " + demonstrateWeightAddition(w1, w2, WeightUnit.POUND));
 	}
 
-	//method to print result
+	// method to print result
 	private static void printResult(String label, Length result) {
 		System.out.println(label + " = " + result);
 	}
+
 }
