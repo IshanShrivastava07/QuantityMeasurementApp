ac42a1626ef25332e460374359058624cc518cf4
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2026-02-27 16:11:25 +0530

    [Ishan Shrivastava] : added Weight Unit in Quantity Measurement

---
+++ src/main/java/com/apps/quantitymeasurement/WeightUnit.java
@@ -0,0 +1,26 @@
+package com.apps.quantitymeasurement;
+
+public enum WeightUnit {
+	KILOGRAM(1.0), // Base unit
+	GRAM(0.001), POUND(0.453592);
+
+	private final double conversionFactor;
+
+	WeightUnit(double conversionFactor) {
+		this.conversionFactor = conversionFactor;
+	}
+
+	public double getConversionFactor() {
+		return conversionFactor;
+	}
+
+	// Convert value in this unit → kilograms (base unit)
+	public double convertToBaseUnit(double value) {
+		return value * conversionFactor;
+	}
+
+	// Convert value from kilograms → this unit
+	public double convertFromBaseUnit(double baseValue) {
+		return baseValue / conversionFactor;
+	}
+}
