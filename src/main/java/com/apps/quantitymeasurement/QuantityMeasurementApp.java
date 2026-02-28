2766af9c4bfeeb3c7ec41cb76147785ffd91dd88
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2026-02-28 10:49:32 +0530

    [Ishan Shrivastava] : modified Quantity Measurement App

--- src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -16,6 +16,17 @@ public class QuantityMeasurementApp {
 		return q1.add(q2, targetUnit);
 	}
 
+	// Subtraction
+	public static <U extends IMeasurable> Quantity<U> demonstrateSubtraction(Quantity<U> q1, Quantity<U> q2,
+			U targetUnit) {
+		return q1.subtract(q2, targetUnit);
+	}
+
+	// Division
+	public static <U extends IMeasurable> double demonstrateDivision(Quantity<U> q1, Quantity<U> q2) {
+		return q1.divide(q2);
+	}
+
 	public static void main(String[] args) {
 
 		// LENGTH
@@ -24,19 +35,25 @@ public class QuantityMeasurementApp {
 		System.out.println("Length Equality: " + demonstrateEquality(length1, length2));
 		System.out.println("Length Conversion: " + demonstrateConversion(length1, LengthUnit.INCHES));
 		System.out.println("Length Addition: " + demonstrateAddition(length1, length2, LengthUnit.FEET));
+		System.out.println("Length Subtraction: " + demonstrateSubtraction(length1, length2, LengthUnit.FEET));
+		System.out.println("Length Division: " + demonstrateDivision(length1, length2));
 
 		// WEIGHT
 		Quantity<WeightUnit> weight1 = new Quantity<>(1.0, WeightUnit.KILOGRAM);
 		Quantity<WeightUnit> weight2 = new Quantity<>(1000.0, WeightUnit.GRAM);
-		System.out.println("Weight Equality: " + demonstrateEquality(weight1, weight2));
+		System.out.println("\nWeight Equality: " + demonstrateEquality(weight1, weight2));
 		System.out.println("Weight conversion: " + demonstrateConversion(weight1, WeightUnit.GRAM));
 		System.out.println("Weight Addition: " + demonstrateAddition(weight1, weight2, WeightUnit.KILOGRAM));
-		
+		System.out.println("Weight Subtraction: " + demonstrateSubtraction(weight1, weight2, WeightUnit.KILOGRAM));
+		System.out.println("Weight Division: " + demonstrateDivision(weight1, weight2));
+
 		// VOLUME
 		Quantity<VolumeUnit> volume1 = new Quantity<>(1.0, VolumeUnit.LITRE);
 		Quantity<VolumeUnit> volume2 = new Quantity<>(25.0, VolumeUnit.GALLON);
-		System.out.println("Volume Equality: " + demonstrateEquality(volume1, volume2));
+		System.out.println("\nVolume Equality: " + demonstrateEquality(volume1, volume2));
 		System.out.println("Volume conversion: " + demonstrateConversion(volume1, VolumeUnit.MILLILITRE));
-		
+		System.out.println("Volume Subtraction: " + demonstrateSubtraction(volume1, volume2, VolumeUnit.LITRE));
+		System.out.println("Volume Division: " + demonstrateDivision(volume1, volume2));
+
 	}
 }
